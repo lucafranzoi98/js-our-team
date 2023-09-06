a88e68430b5f5b657e3b140d3a5c1c97c758020b
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-06 16:27:43 +0200

    add working form with Dom Element

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+// Array of objects
 const team = [
    {nome: "Wayne Barnett", ruolo: "Founder & CEO", foto: "./assets/img/wayne-barnett-founder-ceo.jpg"},
    {nome: "Angela Caroll", ruolo: "Chief Editor", foto: "./assets/img/angela-caroll-chief-editor.jpg"},
@@ -9,6 +10,7 @@ const team = [
 
 const rowEl = document.querySelector(".row");
 
+// Cicle to adding dynamically all the value of the objects in the array
 for (let i = 0; i < team.length; i++) {
    const member = team[i];
    const markupEl =
@@ -24,3 +26,43 @@ for (let i = 0; i < team.length; i++) {
    rowEl.insertAdjacentHTML("beforeend", markupEl)
 }
 
+const buttonEl = document.querySelector("button");
+
+// Constant used to change dynamically the image of new members in Lorem Picsum
+let i = 1;
+
+buttonEl.addEventListener("click", function(e){
+
+   // Get the input fields
+   const newName = document.getElementById("nameInput");
+   const newSurname = document.getElementById("surnameInput");
+   const newJob = document.getElementById("ruoloInput");
+   const newImg = `https://picsum.photos/400/429?random=${i}`;
+
+   // Control if the form is empty
+   if (newName.value != "" && newSurname.value != "" && newJob.value != "") {
+      const markupEl =
+      `<div class="col-12 col-md-3">
+         <div class="card border-0 shadow">
+            <img src="${newImg}" class="card-img-top">
+            <div class="card-body">
+               <h4 class="card-title">${newName.value} ${newSurname.value}</h4>
+               <div class="card-text">${newJob.value}</div>
+            </div>
+         </div>
+      </div>`;
+      rowEl.insertAdjacentHTML("beforeend", markupEl);
+      
+      // Reset input fields
+      newName.value = "";
+      newSurname.value = "";
+      newJob.value = "";
+
+      // Increment i to change image
+      i++;
+   } else {
+      alert("Fill all the fields!")
+   }
+
+   e.preventDefault();
+});
