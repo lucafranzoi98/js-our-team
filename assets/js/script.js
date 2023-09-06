15cec89b3e7061e237044d2fd8ae952e04980b47
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-06 13:37:07 +0200

    milestone 0

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+const team = [
+   {nome: "Wayne Barnett", ruolo: "Founder & CEO", foto: "./assets/img/wayne-barnett-founder-ceo.jpg"},
+   {nome: "Angela Caroll", ruolo: "Chief Editor", foto: "./assets/img/angela-caroll-chief-editor.jpg"},
+   {nome: "Walter Gordon", ruolo: "Office Manager", foto: "./assets/img/walter-gordon-office-manager.jpg"},
+   {nome: "Angela Lopez", ruolo: "Social Media Manager", foto: "./assets/img/angela-lopez-social-media-manager.jpg"},
+   {nome: "Scott Estrada", ruolo: "Developer", foto: "./assets/img/scott-estrada-developer.jpg"},
+   {nome: "Barbara Ramos", ruolo: "Graphic Designer", foto: "./assets/img/barbara-ramos-graphic-designer.jpg"}
+];
