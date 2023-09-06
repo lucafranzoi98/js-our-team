fb644ca2137e129ad56f7d333ebb3f6010e276ff
lucafranzoi98 <lucafranzoi98@gmail.com>
2023-09-06 15:04:52 +0200

    bonus 2 + css

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,3 @@
-const divEl = document.querySelector("div");
 const team = [
    {nome: "Wayne Barnett", ruolo: "Founder & CEO", foto: "./assets/img/wayne-barnett-founder-ceo.jpg"},
    {nome: "Angela Caroll", ruolo: "Chief Editor", foto: "./assets/img/angela-caroll-chief-editor.jpg"},
@@ -8,15 +7,21 @@ const team = [
    {nome: "Barbara Ramos", ruolo: "Graphic Designer", foto: "./assets/img/barbara-ramos-graphic-designer.jpg"}
 ];
 
+const rowEl = document.querySelector(".row");
+
 for (let i = 0; i < team.length; i++) {
    const member = team[i];
-   
-   for (const key in member) {
+   const markupEl =
+   `<div class="col-12 col-md-4">
+      <div class="card border-0 shadow">
+         <img src="${member["foto"]}" class="card-img-top">
+         <div class="card-body">
+            <h4 class="card-title">${member["nome"]}</h4>
+            <div class="card-text">${member["ruolo"]}</div>
+         </div>
+      </div>
+   </div>`;
+   rowEl.insertAdjacentHTML("beforeend", markupEl)
 
-      if (key == "foto") {
-         divEl.insertAdjacentHTML("beforeend", `<img src="${member[key]}">`);
-      } else {
-         divEl.insertAdjacentHTML("beforeend", `<div>${member[key]}</div>`);
-      }
-   }
 }
+
